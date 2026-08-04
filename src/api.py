2eb9b9b13b34fb0c06ba86b33a5f97a9adb3e65d
Marius Malong <mariusmalong@gmail.com>
2026-08-04 16:56:51 +1000

    Add FastAPI

--- src/api.py
+++ src/api.py
@@ -1,1 +1,13 @@
 from fastapi import FastAPI
+from src.predict import predict_match
+
+app = FastAPI()
+
+@app.get("/predict")
+def get_prediction(rank_diff: float, points_diff: float, points_data_missing: int,
+                   career_win_rate_diff: float, surface_win_rate_diff: float,
+                   recent_form_diff: float, surface_clay: bool, surface_grass: bool,
+                   surface_hard: bool):
+    return predict_match(rank_diff, points_diff, points_data_missing,
+                         career_win_rate_diff, surface_win_rate_diff,
+                         recent_form_diff, surface_clay, surface_grass, surface_hard)
